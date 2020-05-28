9686243339e4a5ad5462f9dabeb59d97dcb99a2a
irinaKay <0816aszx@gmail.com>
2020-05-29 03:27:03 +0900

    something

--- C/선행검색수정.c
+++ C/선행검색수정.c
@@ -15,22 +15,27 @@ void main()
     }
     database[i] = (rand() % 10)+1;
 
-    printf("\n\n한 가지 값을 바꿀 수 있습니다. 바꾸시겠습니까?( 1 or 0 )\n");
-    scanf("%d", &input);
+    printf("\n\n한 가지 값을 바꿀 수 있습니다. 바꾸시겠습니까?( 변환을 원하시면 1을 입력 )\n");
+    scanf(" %d", &input);
 
-    if(input = 1){
+    if(input == 1){
         printf("바꿀 자리 입력: ");
         scanf("%d", &change);
         if(change > howmany || change < 0){
             printf("\n존재하지 않는 자리");
+            printf("\n값을 변환하지 않습니다.");
         }
         else{
             printf("\n바뀔 숫자 입력: ");
             scanf("%d", &trans);
 
-            database[change] = trans;
+            database[change-1] = trans;
             printf("\n변환 완료");
+
         }
+    if(input != 1){
+        printf("\n값을 변환하지 않습니다.");
+		}
     }
 
     printf("\n\n찾을 값을 입력하세요: ");
