83e4a08f921a385afc0c4265ee744eb2ffc7d21a
irinaKay <0816aszx@gmail.com>
2020-06-19 12:40:18 +0900

    BOJ

--- C/BOJ/BOJ_2446.c
+++ C/BOJ/BOJ_2446.c
@@ -15,16 +15,16 @@ int main() {
 		
 		printf("\n");
 	}
-	
-	for(i=2;i<=n;i++){
-		
-		for(j=1;j<=n-i;j++)
-			printf(" ");
-
-		for(k=0;k<=2*(i-1);k++)
-				printf("*");
-		
-		printf("\n");
-	}
+//	
+//	for(i=2;i<=n;i++){
+//		
+//		for(j=1;j<=n-i;j++)
+//			printf(" ");
+//
+//		for(k=0;k<=2*(i-1);k++)
+//				printf("*");
+//		
+//		printf("\n");
+//	}
 }
 
