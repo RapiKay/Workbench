e9fe1d1d7397061523d0d4761e8e83a495135986
BuildTools <unconfigured@null.spigotmc.org>
2020-08-31 09:23:25 +0900

    0831

---
+++ C/BOJ/10872.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int fact(int num){
+    if(num > 1)
+        return num*fact(num-1);
+    else
+        return 1;    
+}
+	//.
+int main()
+{
+    int N;
+
+    scanf("%d",&N);
+    printf("%d\n",fact(N));
+    
+    return 0;
+}
